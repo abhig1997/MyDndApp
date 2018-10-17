601c9c95e71da8f2b09b380653011b0c07e7e8fa
abhig1997 <gupta257@purdue.edu>
2018-10-17 14:58:16 -0400

    added a bunch of stuff to the char creation screen

--- app/src/main/java/com/abhig1997/mydndapp/CreateCharacterScreen.java
+++ app/src/main/java/com/abhig1997/mydndapp/CreateCharacterScreen.java
@@ -11,6 +11,37 @@ public class CreateCharacterScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_character_screen);
+//        Spinner spinner = (Spinner) findViewById(R.id.class_spinner);
+//        // Create an ArrayAdapter using the string array and a default spinner layout
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//                R.array.classes_array, android.R.layout.simple_spinner_item);
+//        // Specify the layout to use when the list of choices appears
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        // Apply the adapter to the spinner
+//        spinner.setAdapter(adapter);
+        createClassesSpinner();
+        createAlignmentSpinner();
+
+    }
+
+    /**
+     * Create the entries for the alignment drop down menu
+     */
+    private void createAlignmentSpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.alignment_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.alignment_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+    }
+
+    /**
+     * Create the entries for the classes spinner
+     */
+    public void createClassesSpinner() {
         Spinner spinner = (Spinner) findViewById(R.id.class_spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -19,7 +50,6 @@ public class CreateCharacterScreen extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
-
     }
 
 
