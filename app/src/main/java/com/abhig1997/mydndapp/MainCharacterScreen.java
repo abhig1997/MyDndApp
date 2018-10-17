e6057e653a729909e59305f83db2756158bbd53c
abhig1997 <gupta257@purdue.edu>
2018-10-17 14:12:52 -0400

    added main character screen activity, set it as the default activity

---
+++ app/src/main/java/com/abhig1997/mydndapp/MainCharacterScreen.java
@@ -0,0 +1,18 @@
+package com.abhig1997.mydndapp;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainCharacterScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_character_screen);
+
+        // hide the action bar
+        ActionBar ab = getSupportActionBar();
+        ab.hide();
+    }
+}
