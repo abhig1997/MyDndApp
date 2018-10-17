453eee0b9af45b5d765a9bbf5f6ef8d127c5c96a
abhig1997 <gupta257@purdue.edu>
2018-10-17 14:21:04 -0400

    added a screen to create character

---
+++ app/src/main/java/com/abhig1997/mydndapp/CreateCharacterScreen.java
@@ -0,0 +1,15 @@
+package com.abhig1997.mydndapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateCharacterScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_character_screen);
+    }
+
+    
+}
--- app/src/main/java/com/abhig1997/mydndapp/MainCharacterScreen.java
+++ app/src/main/java/com/abhig1997/mydndapp/MainCharacterScreen.java
@@ -1,8 +1,10 @@
 package com.abhig1997.mydndapp;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainCharacterScreen extends AppCompatActivity {
 
@@ -12,7 +14,24 @@ public class MainCharacterScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_character_screen);
 
         // hide the action bar
-        ActionBar ab = getSupportActionBar();
-        ab.hide();
+//        ActionBar ab = getSupportActionBar();
+//        ab.hide();
+    }
+
+    /**
+     * Called when the user taps the Create Character button
+     * @param view
+     */
+    public void createCharacter(View view) {
+        Intent intent = new Intent(this, CreateCharacterScreen.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Called when the user taps the View Characters button
+     * @param view
+     */
+    public void viewCharacters(View view) {
+
     }
 }
