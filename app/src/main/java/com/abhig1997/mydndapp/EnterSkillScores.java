cf99c2b9912b3c92896d2150ed112a737a5b34c1
abhig1997 <gupta257@purdue.edu>
2018-12-18 23:42:33 -0800

    added some skill score boxes

---
+++ app/src/main/java/com/abhig1997/mydndapp/EnterSkillScores.java
@@ -0,0 +1,13 @@
+package com.abhig1997.mydndapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnterSkillScores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_skill_scores);
+    }
+}
