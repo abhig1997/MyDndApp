166e79abbce1ae52775cf45d7d6a1a7dadf46038
abhig1997 <gupta257@purdue.edu>
2018-12-27 21:09:28 -0800

    fixed wrong input type in final stats screen, added intent code

--- app/src/main/java/com/abhig1997/mydndapp/EnterFinalStats.java
+++ app/src/main/java/com/abhig1997/mydndapp/EnterFinalStats.java
@@ -1,7 +1,10 @@
 package com.abhig1997.mydndapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class EnterFinalStats extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class EnterFinalStats extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enter_final_stats);
+    }
+
+    public void submitFinalStats(View view) {
+        Intent intent = new Intent(this, CharacterView.class); // create the intent
+                                                                        // for the next screen
+
+        Bundle updatedExtras = getIntent().getExtras(); // need the bundle to update it with
+                                                        // new info
+
+        // add the final stats
+        EditText hp_box = (EditText) findViewById(R.id.hp_box);
+        int hp_score = Integer.parseInt(hp_box.getText().toString());
+        updatedExtras.putInt("HIT_POINTS", hp_score);
+
+
+        intent.putExtras(updatedExtras);
+
+        startActivity(intent);
     }
 }
