a344584fa36e33675ac1f9c43fed66b41066c4d7
abhig1997 <gupta257@purdue.edu>
2018-12-31 19:34:45 -0800

    completed setup for loading of data, need to transfer from JSON to bundle

--- app/src/main/java/com/abhig1997/mydndapp/ShowCharacterSaves.java
+++ app/src/main/java/com/abhig1997/mydndapp/ShowCharacterSaves.java
@@ -1,9 +1,23 @@
 package com.abhig1997.mydndapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 public class ShowCharacterSaves extends AppCompatActivity {
 
@@ -31,6 +45,64 @@ public class ShowCharacterSaves extends AppCompatActivity {
     public String[] getFiles() {
         System.out.println(fileList());
         return fileList();
+    }
+
+    /**
+     * Called when the submit button is clicked
+     * @param view
+     */
+    public void goToCharacterScreen(View view) {
+        // get the file they selected
+        Spinner saveSpinner = (Spinner) findViewById(R.id.saveSpinner); // the spinner in the view
+        String filename = saveSpinner.getSelectedItem().toString();
+
+        if (filename == null || filename.length() == 0) {
+            // bad filename or no saves exist
+            return;
+        }
+
+        // load the data from the file
+        String json = null;
+        try {
+            InputStream is = openFileInput(filename);
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, "UTF-8");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (json == null) {
+            // couldn't load a string for some reason
+            System.out.println("bad filename, could not load json object");
+            return;
+        }
+
+
+
+        try {
+            JSONObject obj = new JSONObject(json);
+
+            // need to get all the values from the json file and put them in a bundle
+            // then pass that bundle to the CharacterView page
+            Bundle extras = new Bundle(); // the bundle to pass to the next activity
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 
