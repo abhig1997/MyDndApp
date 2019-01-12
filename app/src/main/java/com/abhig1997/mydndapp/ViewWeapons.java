ca2e496dd227bc6a94f8da6b92795cc1bbe5f294
abhig1997 <gupta257@purdue.edu>
2019-01-12 13:36:29 -0500

    added weapons activity

---
+++ app/src/main/java/com/abhig1997/mydndapp/ViewWeapons.java
@@ -0,0 +1,13 @@
+package com.abhig1997.mydndapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewWeapons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_weapons);
+    }
+}
